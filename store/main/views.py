07cfd8fe7b3f609a9cf57613c9769b6336196e3c
danchous21 <157131237+danchous21@users.noreply.github.com>
2024-12-10 19:32:04 +0300

    первые наброски главной страницы

---
+++ store/main/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Какой классный магазин'
+    }
+
+    return render(request, 'main/about.html', context)
