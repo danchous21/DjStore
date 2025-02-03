5f329e1c1ca2129203b56702acaade7dffe5d46e
danchous21 <157131237+danchous21@users.noreply.github.com>
2025-02-03 15:29:20 +0300

    добавлена модель

--- store/goods/views.py
+++ store/goods/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 
 def catalog(request):
+    content = {
+        'title':"Home - Каталог",
+        'goods':'my'
+    }
     return render(request, 'goods/catalog.html')
 
 def product(request):
